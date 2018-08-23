aeb148bd252b53d89fd69cd82ced98d28dc1dafe
JoseEdeNeto <jose.ede.neto@gmail.com>
2018-08-23 20:35:05 -0300

    teste Vetor Desordenado Par Mediana

--- src/test/java/tdd/CalculadoraTest.java
+++ src/test/java/tdd/CalculadoraTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 public class CalculadoraTest {
    
    @Test
-   public void testeVetorOrdenadoImpar(){
+   public void testeVetorOrdenadoImparMediana(){
        double vetor[] = {1, 2, 3, 4, 5};
        Calculadora mediana = new Calculadora();;
        double res = mediana.calculaMediana(vetor);
@@ -21,4 +21,12 @@ public class CalculadoraTest {
        Assert.assertEquals(3, res, 0.1);
    }
    
+   @Test
+   public void testeVetorDesordenadoParMediana(){
+       double vetor[] = {5, 2, 1, 4, 3, 6};
+       Calculadora mediana = new Calculadora();;
+       double res = mediana.calculaMediana(vetor);
+       
+       Assert.assertEquals(3.5, res, 0.1);
+   }
 }
