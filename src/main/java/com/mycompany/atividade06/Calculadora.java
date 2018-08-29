f737cbb21eb9c5e98d155bc76a19d66c55f15d9d
José Ede de Campos Neto <jose.ede.neto@gmail.com>
2018-08-29 15:54:29 -0300

    passa teste Vetor Desordenado Par Mediana

--- src/main/java/com/mycompany/atividade06/Calculadora.java
+++ src/main/java/com/mycompany/atividade06/Calculadora.java
@@ -1,12 +1,20 @@
 package com.mycompany.atividade06;
 
+import java.util.Arrays;
+
 public class Calculadora {
     
     public double calculaMediana(double vetor[]){
+        Arrays.sort(vetor);
         double mediana;
         int tamanho = vetor.length;
-        mediana = vetor[tamanho/2];
-        
+        if(tamanho % 2 == 1){
+            mediana = vetor[tamanho / 2];
+        } else {
+            int valor1 = (vetor.length / 2) - 1;
+            int valor2 = (vetor.length / 2);
+            mediana = (vetor[valor1]+vetor[valor2]) / 2;
+        }
         return mediana;
     }
 }
