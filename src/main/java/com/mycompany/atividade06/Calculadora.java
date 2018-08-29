139d07ba88e2fd50e20c328278200e7dae0a26b7
José Ede de Campos Neto <jose.ede.neto@gmail.com>
2018-08-29 18:03:04 -0300

    passa teste Media Valores Menor Maior Acima Abaixo

--- src/main/java/com/mycompany/atividade06/Calculadora.java
+++ src/main/java/com/mycompany/atividade06/Calculadora.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Calculadora {
     
-    public double calculaMediana(double vetor[]){
-        Arrays.sort(vetor);
+    public double calculaMediana(double[] vetor){
+        ordenaVetor(vetor);
         double mediana;
         int tamanho = vetor.length;
         if(tamanho % 2 == 1){
@@ -16,5 +16,49 @@ public class Calculadora {
             mediana = (vetor[valor1]+vetor[valor2]) / 2;
         }
         return mediana;
+    }
+    
+    public double calculaMedia(double[] vetor){
+        ordenaVetor(vetor);
+        double media = 0.0;
+        for (double valor : vetor){
+            media += valor;
+        }
+        media /= vetor.length;
+        return media;
+    }
+
+    public double getMenorValor(double[] vetor) {
+        ordenaVetor(vetor);
+        return vetor[0];
+    }
+    public double getMaiorValor(double[] vetor) {
+        ordenaVetor(vetor);
+        return vetor[vetor.length - 1];
+    }
+
+    public int getValoresAbaixo(double[] vetor, double media) {
+        ordenaVetor(vetor);
+        int count = 0;
+        for(double valor : vetor){
+            if (valor < media)
+                count++;
+        }
+        return count;
+    }
+
+    public int getValoresAcima(double[] vetor, double media) {
+        ordenaVetor(vetor);
+        int count = 0;
+        for(double valor : vetor){
+            if (valor > media)
+                count++;
+        }
+        return count;
+    }
+    
+    private double[] ordenaVetor(double[] vetor){
+        Arrays.sort(vetor);
+        return vetor;
     }
 }
