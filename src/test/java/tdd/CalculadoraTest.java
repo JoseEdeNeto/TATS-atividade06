5ee2bd0f369d6f621e159f27c0fe73457011a9f5
José Ede de Campos Neto <jose.ede.neto@gmail.com>
2018-08-29 18:02:36 -0300

    teste Media Valores Menor Maior Acima Abaixo

--- src/test/java/tdd/CalculadoraTest.java
+++ src/test/java/tdd/CalculadoraTest.java
@@ -1,20 +1,14 @@
 package tdd;
 
 import com.mycompany.atividade06.Calculadora;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-
 
 public class CalculadoraTest {
    
    @Test
    public void testeVetorOrdenadoImparMediana(){
-       double vetor[] = {1, 2, 3, 4, 5};
+       double[] vetor = {1, 2, 3, 4, 5};
        Calculadora mediana = new Calculadora();;
        double res = mediana.calculaMediana(vetor);
        
@@ -23,10 +17,27 @@ public class CalculadoraTest {
    
    @Test
    public void testeVetorDesordenadoParMediana(){
-       double vetor[] = {5, 2, 1, 4, 3, 6};
+       double[] vetor = {5, 2, 1, 4, 3, 6};
        Calculadora mediana = new Calculadora();;
        double res = mediana.calculaMediana(vetor);
        
        Assert.assertEquals(3.5, res, 0.1);
+   }
+   
+   @Test
+   public void testMediaValoresMenorMaiorAcimaAbaixo(){
+       double[] vetor = {5, 2, 1, 4, 3, 6};
+       Calculadora calc = new Calculadora();
+       double media = calc.calculaMedia(vetor);
+       double menorValor = calc.getMenorValor(vetor);
+       double maiorValor = calc.getMaiorValor(vetor);
+       int numerosAbaixo = calc.getValoresAbaixo(vetor, media);
+       int numerosAcima = calc.getValoresAcima(vetor, media);
+       
+       Assert.assertEquals(3.5, media, 0.1);
+       Assert.assertEquals(1, menorValor, 0.1);
+       Assert.assertEquals(6, maiorValor, 0.1);
+       Assert.assertEquals(3, numerosAbaixo);
+       Assert.assertEquals(3, numerosAcima);
    }
 }
